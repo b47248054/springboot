3302026f11f987fd1ca917d93d5843774748dd54
wanye <wanye@meili-inc.com>
2017-05-24 22:36:45 +0800

    Spring boot 拦截器

--- src/main/java/com/wanye/interceptor/HelloInterceptor.java
+++ src/main/java/com/wanye/interceptor/HelloInterceptor.java
@@ -12,16 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return false;
+        System.out.println(">>interceptor preHandle<<");
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
+        System.out.println(">>interceptor postHandle<<");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
+        System.out.println(">>interceptor afterCompletion<<");
     }
 }
